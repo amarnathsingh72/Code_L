0131515ff3215fd13169e9666d62c22eea94f9f6
SINGH <amarnathsingh10122006@gmail.com>
2026-08-16 00:50:39 +0530

    3090 Done

---
+++ 3090.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maximumLengthSubstring(string s) {
+        int freq[26] = {0};
+        int left = 0;
+        int ans = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            // Add current character to the window
+            freq[s[right] - 'a']++;
+
+            // If any character occurs more than 2 times,
+            // shrink the window from the left
+            while (freq[s[right] - 'a'] > 2) {
+                freq[s[left] - 'a']--;
+                left++;
+            }
+
+            // Current window is valid
+            ans = max(ans, right - left + 1);
+        }
+     return ans;
+    }
+};
