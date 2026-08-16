199cb1843c06e9fea84b90919fbefe8efddb50fb
amarnathsingh72 <amarnathsingh10122006@gmail.com>
2026-08-17 00:19:12 +0530

    Time: 1 ms (82.74%), Space: 131.3 MB (33.5%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int cnt[3] = {};
+
+        for (int x : stones) {
+            ++cnt[x % 3];
+        }
+
+        // If one of the remainder-1/remainder-2 groups is empty,
+        // Alice is forced to start with the other group.
+        if (min(cnt[1], cnt[2]) == 0) {
+            return max(cnt[1], cnt[2]) > 2 && cnt[0] % 2 == 1;
+        }
+
+        // Both remainder types exist.
+        return abs(cnt[1] - cnt[2]) > 2 || cnt[0] % 2 == 0;
+    }
+};
