ec865b5673507c6ecf259c42cc1d81a489c54da9
amarnathsingh72 <amarnathsingh10122006@gmail.com>
2026-08-19 01:39:28 +0530

    Time: 4 ms (60.7%), Space: 131.4 MB (29.75%) - LeetHub

--- 2029-stone-game-ix/2029-stone-game-ix.cpp
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -7,8 +7,8 @@ public:
             ++cnt[x % 3];
         }
         // If one of the remainder-1/remainder-2 groups is empty,
-        // Alice is forced to start with the other group.
-        
+        // Alice is forced to start with the other group.  
+         
         if (min(cnt[1], cnt[2]) == 0) {
             return max(cnt[1], cnt[2]) > 2 && cnt[0] % 2 == 1;
         }
