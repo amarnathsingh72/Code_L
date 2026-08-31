46eda2b042500b141a3dc7e35234862013886ceb
amarnathsingh72 <amarnathsingh10122006@gmail.com>
2026-08-31 14:28:49 +0530

    Time: 0 ms (100%), Space: 19.2 MB (79.97%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        arr=nums
+        l=0
+        r=len(arr)-1
+        while l<r:
+            m=(l+r)//2
+            if arr[m]<arr[m+1]:
+                l=m+1
+            else:
+                r=m
+        return r #L or R both are okay.  12345431ismai mid lete aur mid ke pehelr toh ans nahi rehta, then aage kuch karte
