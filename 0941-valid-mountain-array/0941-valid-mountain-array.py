dce941a8f565ac2514b3ba49103125b56f90c162
amarnathsingh72 <amarnathsingh10122006@gmail.com>
2026-08-31 14:01:46 +0530

    Time: 164 ms (35.47%), Space: 20.4 MB (96.96%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.py
+++ 0941-valid-mountain-array/0941-valid-mountain-array.py
@@ -3,7 +3,7 @@ class Solution:
         i=0
         while i<len(arr)-1 and arr[i]<arr[i+1]:
             i=i+1
-        if i==0 or i==len(arr)-1:
+        if i==0 or i==len(arr)-1: #agae shuru mai he 0 raha tho isliye condition lagaye// or bas value upper jara but niche nahi aara bole tho
             return False
         while i<len(arr)-1 and arr[i]>arr[i+1]:
             i=i+1
@@ -11,3 +11,4 @@ class Solution:
             return True
         else:
             return False
+# ismaitu bas increasing dekra jap teku peak value milta vaha se value decrease dek na. Agar valur increase hora and then decrease hjora bole tho wo mountain hai. 
