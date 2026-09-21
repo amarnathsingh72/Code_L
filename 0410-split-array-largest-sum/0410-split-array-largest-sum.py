871963785476e2d112b183a3d764b576de360de1
SSingh_72 <amarnathsingh10122006@gmail.com>
2026-09-21 14:28:11 +0530

    Time: 3 ms (71.31%), Space: 19.2 MB (96.81%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.py
@@ -0,0 +1,29 @@
+class Solution:
+    def splitArray(self, weights: list[int], days: int) -> int:
+        def fun(c):
+            day = 1
+            total = 0
+
+            for i in weights:
+                if total + i <= c:
+                    total += i
+                else:
+                    day += 1
+                    total = i
+
+            return day <= days
+
+        l = max(weights)
+        r = sum(weights)
+        ans = 0
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            if fun(mid):
+                ans = mid
+                r = mid - 1
+            else:
+                l = mid + 1
+
+        return ans
