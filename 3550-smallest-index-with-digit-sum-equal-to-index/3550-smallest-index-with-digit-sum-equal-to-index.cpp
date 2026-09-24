a1189c9dd4c2d506511403639143be56c728ae8c
SSingh_72 <amarnathsingh10122006@gmail.com>
2026-09-24 22:16:46 +0530

    Time: 0 ms (100%), Space: 30.9 MB (52.36%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.cpp
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.cpp
@@ -4,7 +4,7 @@ public:
         for(int i = 0; i < nums.size(); i++){
             int x = nums[i];
             int sum = 0;
-            // extract kar re
+            // extract kar re aur add kar re
             while(x > 0){
                 sum += x % 10;
                 x /= 10;
