e4bead51f287f1011450e0ca1cda392ffdbefaaf
SSingh_72 <amarnathsingh10122006@gmail.com>
2026-09-24 21:45:09 +0530

    Time: 0 ms (100%), Space: 12.2 MB (12.56%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -6,7 +6,7 @@ public:
         int n=strs.size()-1;
         string last=strs[n];
         string res="";
-
+// pehele hum string ko sort karte
         for (int i=0;i<first.length() && i<last.length();i++){
             if(first[i]==last[i]){
                 res=res+first[i];
